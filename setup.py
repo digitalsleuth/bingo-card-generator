05e26d4f356f27c9a088310bb619e7ca18916a17
red <corey@digitalsleuth.ca>
2021-07-17 20:32:08 +0000

    Add setup

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+from setuptools import setup, find_packages
+
+with open("README.md", encoding='utf8') as readme:
+    long_description = readme.read()
+
+setup(
+    name="bingo-card-generator",
+    version="1.0.0",
+    author="Corey Forman",
+    url="https://github.com/digitalsleuth/bingo-card-generator",
+    description=("Interactive Bingo Card Generator"),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "pdfkit"
+    ],
+    scripts=['bingo-card-generator.py'],
+    package_data={'': ['README.md, LICENSE']}
+)
