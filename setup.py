2a6680646153960d9a8a7d181a8c26b8851d31cd
digitalsleuth <corey@digitalsleuth.ca>
2023-02-19 04:36:57 +0000

    Update classifiers

--- setup.py
+++ setup.py
@@ -17,6 +17,7 @@ setup(
     packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     install_requires=[
